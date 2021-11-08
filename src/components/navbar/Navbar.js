909b261fbd6b14526cf30db366689263e54c32d0
Waleed <wsaleh1991@gmail.com>
2021-11-08 07:24:49 -0600

    initial commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+// styles
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>My Money</li>
+        <li>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABoF44H-0WAWyh0MWze_cyChcaEPzx1bY",
+  authDomain: "money-app-b531b.firebaseapp.com",
+  projectId: "money-app-b531b",
+  storageBucket: "money-app-b531b.appspot.com",
+  messagingSenderId: "223241402338",
+  appId: "1:223241402338:web:24a3c4fe030667941f6bbb",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
