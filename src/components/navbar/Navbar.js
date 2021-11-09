5d852fd9ea972086b707a6b473903c04e850b385
Waleed <wsaleh1991@gmail.com>
2021-11-09 06:27:17 -0600

    application deployed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>My Money</li>
+        <li className={styles.title}>My Money App</li>
         {!user && (
           <>
             <li>
